c2ab744d78cbf249bca5c1058151d35935f9165a
Rouven Weßling <rouven@contentful.com>
2017-01-09 11:03:07 +0100

    Generated scaffolding for a Chrome App that runs in kiosk mode.

---
+++ chromeapp/config.js
@@ -0,0 +1,8 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Contentful Office Signage",
+   "enableNavBttns": false,
+   "enableHomeBttn": true,
+   "kioskEnabled": true
+};
