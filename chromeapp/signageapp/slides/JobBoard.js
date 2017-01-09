34fda68321a3e91a019be4b39117c90608b782bd
Rouven Weßling <rouven@contentful.com>
2017-01-10 00:24:38 +0100

    word cloud

--- chromeapp/signageapp/slides/JobBoard.js
+++ chromeapp/signageapp/slides/JobBoard.js
@@ -1,4 +1,9 @@
 import React, { Component, PropTypes } from 'react';
+import { TagCloud } from 'react-tagcloud';
+
+function getRandom(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
 
 
 export default class JobBoard extends Component {
@@ -7,15 +12,20 @@ export default class JobBoard extends Component {
   };
 
   render() {
-    const data = this.props.data;
+    const jobs = this.props.data.jobs.map(function(job) {
+      return {value: job, count: getRandom(1, 50)};
+    });
+    const office = this.props.data.office;
 
-    return(<div>
-        <h1>{data.office}</h1>
-        <ul>
-          {data.jobs.map(function(job) {
-            return <li>{job}</li>;
-          })}
-        </ul>
+    return(<div className={office.toLowerCase().replace(' ', '-')}>
+        <h1>We're hiring!</h1>
+        <h2>For the {office} office</h2>
+        <div className="cloudWrapper">
+          <TagCloud minSize={30}
+                    maxSize={60}
+                    tags={jobs}
+          />
+        </div>
       </div>
       )
   }
